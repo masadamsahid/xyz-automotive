ce084fae50f5103445f19367622ca07aca83cffb
masadamsahid <masadamsahid@gmail.com>
2026-03-11 14:20:51 +0700

    add env example frontend

--- frontend/src/lib/api/edent-treaty.ts
+++ frontend/src/lib/api/edent-treaty.ts
@@ -2,6 +2,7 @@
 import { treaty } from '@elysiajs/eden'
 import type { BackendApp } from '../../../../backend/src'
 
-// @ts-ignore
-export const client = treaty<BackendApp>('localhost:3000') 
+const API_URL = process.env.NEXT_PUBLIC_API_URL || process.env.API_URL || 'localhost:3000'
 
+// @ts-ignore
+export const client = treaty<BackendApp>(API_URL) 
